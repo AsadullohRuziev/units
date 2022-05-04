34d1df549bca5549e530517a408d1e048678f134
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-04 13:42:52 +0500

    View yasash

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ class UserController extends Controller
 {
     public function show($id = null) //idni ixtiyoriy tazda qo'yish mumkin va difault qilish ham mumkin
     {
-        return "user prifile for ID ".$id;
+        return view('show');  //view folderning ichidagi show.blade.php filening manzilini ko'rsatyapti
     }
 }
---
+++ resources/views/show.blade.php
@@ -0,0 +1,5 @@
+<html lang="en">
+<body>
+    <h1>Bu User profile </h1>
+</body>
+</html>

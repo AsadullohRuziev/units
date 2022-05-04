ae6ca34226fe7c70fa1b07279001d06a75e6b6b6
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-05 00:56:16 +0500

    DATABASE

---
+++ resources/views/setup.blade.php
@@ -0,0 +1,11 @@
+
+
+<h1>phpmyadminga borib data yaratamin</h1>
+<h1>.env file togirlaymiz</h1>
+<h1> php artisan tinker</h1>
+<H1>>>> DB::connection()->getPdo();
+    PDOException with message 'SQLSTATE[HY000] [1045] Access denied for user 'mysql'@'localhost' (using password: YES)'
+
+
+    dont fix that section
+</H1>

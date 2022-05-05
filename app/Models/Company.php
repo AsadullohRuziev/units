ebc37e1d70328f614346a446acdd648366b76cd9
AsadullohRuziev <asadullohruziev@gmail.com>
2022-05-05 16:45:22 +0500

    MODEL haqida batafsil

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+
+    //table companies
+    protected $table = "my_companies";  //companiesga emas bu my_comainesga murojat qiladi
+
+
+    //custom primary key
+    protected $primaryKey = 'company_id';
+
+    //incrementing
+    public  $incrementing = false;
+
+
+
+    //timestamps
+    public  $timestamps = false;  //created_at va updated_at  kerak bo'lmaydi
+
+
+    //default values berish
+    protected $attributes = [
+        'active' => true
+    ];
 }
